c85bae353414114a17fd947fb40032d4d434340e
NKH123 <neeraj.deshpande3@gmail.com>
2018-09-01 00:56:24 +0530

    Added template

---
+++ Template/template.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <b; i++)
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+#define llp 1000000007
+
+int main()
+{   ios::sync_with_stdio(false);
+
+    return 0;
+}
