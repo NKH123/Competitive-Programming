f9b62b4af3f4938a6e74550aba271f7e54f5a4ec
Neeraj <neeraj.171it226@nitk.edu.in>
2019-03-04 01:21:49 +0530

    CF round 543

---
+++ Codeforces/1121A.cpp
@@ -0,0 +1,106 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <b; i++)
+typedef long long ll;
+#define int ll
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef pair<int,int> pi;
+#define trace(x) cout<<#x<<"="<<x<<"\n";
+#define llp 1000000007
+#define mod 1000000007
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+    os << "{";
+    typename vector< T > :: const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+    os << "[";
+    typename set< T > :: const_iterator it;
+    for ( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+    os << "[";
+    typename map< F , S >::const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << it -> first << " = " << it -> second ;
+    }
+    return os << "]";
+}
+
+#define deb(x) cerr << #x << " = " << x << endl;
+
+
+int32_t main(){
+	ios::sync_with_stdio(false);
+    int n,m,k;
+    cin>>n>>m>>k;
+    vi po(n),sc(n);
+    REP(i,0,n){
+        cin>>po[i];
+    }
+    REP(i,0,n){
+        cin>>sc[i];
+    }
+    vi kk(k);
+    REP(i,0,k){
+        cin>>kk[i];
+        kk[i]--;
+    }
+    vi mar(n,0);
+
+    for(int i=1;i<=m;i++){
+        ll smalli=-1;
+        ll val=-1;
+        for(int j=0;j<n;j++){
+            if(sc[j]==i){
+                if(smalli==-1){
+                    smalli=j;
+                    val=po[j];
+                    continue;
+                }
+                if(val<po[j]){
+                    smalli=j;
+                    val=po[j];
+                }
+            }
+        }
+        if(smalli!=-1)
+        mar[smalli]=1;
+    }
+    ll ans=k;
+    REP(i,0,k){
+        if(mar[kk[i]]){
+            ans--;
+        }
+    }
+    cout<<ans<<"\n";
+
+
+
+	return 0;
+}
---
+++ Codeforces/1121B.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <b; i++)
+typedef long long ll;
+#define int ll
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef pair<int,int> pi;
+#define trace(x) cout<<#x<<"="<<x<<"\n";
+#define llp 1000000007
+#define mod 1000000007
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+    os << "{";
+    typename vector< T > :: const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+    os << "[";
+    typename set< T > :: const_iterator it;
+    for ( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+    os << "[";
+    typename map< F , S >::const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << it -> first << " = " << it -> second ;
+    }
+    return os << "]";
+}
+
+#define deb(x) cerr << #x << " = " << x << endl;
+
+vi sum(200005);
+
+int32_t main(){
+	ios::sync_with_stdio(false);
+    int n;
+    cin>>n;
+    vi a(n);
+    REP(i,0,n)cin>>a[i];
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            sum[a[i]+a[j]]++;
+        }
+    }
+    ll ans=0;
+    REP(i,0,200004){
+        ans=max(ans,sum[i]);
+    }
+    cout<<ans<<"\n";
+
+
+
+	return 0;
+}
