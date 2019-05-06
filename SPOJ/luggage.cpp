363c22bf3558b111f9412565e3292126397bf4ac
Neeraj <neeraj.171it226@nitk.edu.in>
2019-05-06 12:01:15 +0530

    Some OJ contest

---
+++ SPOJ/luggage.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <b; i++)
+typedef long long ll;
+#define int ll
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef pair<int,int> pi;
+#define trace(x) cout<<#x<<"="<<x<<"\n";
+#define llp 1000000007
+#define mod 1000000007
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+    os << "{";
+    typename vector< T > :: const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+    os << "[";
+    typename set< T > :: const_iterator it;
+    for ( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+    os << "[";
+    typename map< F , S >::const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << it -> first << " = " << it -> second ;
+    }
+    return os << "]";
+}
+
+#define deb(x) cerr << #x << " = " << x << endl;
+
+
+int32_t main(){
+	ios::sync_with_stdio(false);
+    int n,k;
+    cin>>n>>k;
+    vi a(n);
+    REP(i,0,n)cin>>a[i];
+    sort(a.begin(),a.end());
+    int ans=-1;
+   // deb(a);
+    for(int i=0;i<n;i++){
+       // deb(i);
+        auto g=upper_bound(a.begin(),a.end(),a[i]+k);
+        g--;
+        if((*g)-a[i]>k){
+            continue;
+        }
+          if(g==a.end()){
+            break;
+        }
+        ans=max(ans,int(g-a.begin()-i+1));
+        //cout<<a[i]<<" "<<*g<<" "<<"range is "<<int(g-a.begin()-i)<<"\n";
+      
+    }
+    cout<<ans<<"\n";
+
+
+
+	return 0;
+}
