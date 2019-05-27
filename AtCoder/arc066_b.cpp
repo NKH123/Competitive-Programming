09e940b9905b3ba81d61f31f51ff2928e5f503ca
Neeraj <neeraj.171it226@nitk.edu.in>
2019-05-28 02:50:03 +0530

    Cool digit DP problem

---
+++ AtCoder/arc066_b.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <b; i++)
+typedef long long ll;
+#define int ll
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef pair<int,int> pi;
+#define trace(x) cout<<#x<<"="<<x<<"\n";
+#define llp 1000000007
+#define mod 1000000007
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+    os << "{";
+    typename vector< T > :: const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+    os << "[";
+    typename set< T > :: const_iterator it;
+    for ( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+    os << "[";
+    typename map< F , S >::const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << it -> first << " = " << it -> second ;
+    }
+    return os << "]";
+}
+
+#define deb(x) cerr << #x << " = " << x << endl;
+map <ll, int> dp;
+int DP(ll n) {
+    if (n < 0) return 0;
+    if (n == 0) return 1;
+    if (dp.count(n)) return dp[n];
+    int res = 0;
+    if (n & 1) res = (DP(n / 2) * 2 % mod + DP(n / 2 - 1)) % mod;
+    else res = (DP(n / 2) + DP(n / 2 - 1) * 2 % mod) % mod;
+    return dp[n] = res;
+}
+
+int32_t main(){
+	ios::sync_with_stdio(false);
+	ll n;
+    cin >> n;
+    cout << DP(n) << endl;
+   	return 0;
+}
