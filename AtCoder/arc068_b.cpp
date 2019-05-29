99136de91fc2022da6a6ac129f5b5fcd1f3c7ec6
Neeraj <neeraj.171it226@nitk.edu.in>
2019-05-29 15:54:13 +0530

    D Practice

---
+++ AtCoder/arc068_b.cpp
@@ -0,0 +1,96 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <b; i++)
+typedef long long ll;
+#define int ll
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef pair<int,int> pi;
+#define trace(x) cout<<#x<<"="<<x<<"\n";
+#define llp 1000000007
+#define mod 1000000007
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+    os << "{";
+    typename vector< T > :: const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+    os << "[";
+    typename set< T > :: const_iterator it;
+    for ( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+    os << "[";
+    typename map< F , S >::const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << it -> first << " = " << it -> second ;
+    }
+    return os << "]";
+}
+
+#define deb(x) cerr << #x << " = " << x << endl;
+
+
+int32_t main(){
+	ios::sync_with_stdio(false);
+    int n;
+    cin>>n;
+    vi a(n);
+    vi b(100005,0);
+    REP(i,0,n){
+        cin>>a[i];
+        b[a[i]]++;
+    }
+    set<int>eve,odd;
+    for(int i=0;i<100005;i++){
+        if(b[i]!=0){
+            if(b[i]%2){
+                odd.insert(i);
+                b[i]=1;
+            }
+            else{
+                eve.insert(i);
+                b[i]=2;
+            }
+        }
+    }
+    int ans=0;
+    ans+=odd.size();
+    if(eve.size()%2==0){
+        ans+=(eve.size());
+    }
+    else{
+        ans+=(eve.size());
+        ans--;
+    }
+    cout<<ans<<"\n";
+
+
+
+
+    return 0;
+}
