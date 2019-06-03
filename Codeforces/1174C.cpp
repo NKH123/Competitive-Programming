ed30dc47f5765e3b1fd8c3d0a6f253dfda9280e9
Neeraj <neeraj.171it226@nitk.edu.in>
2019-06-03 23:56:08 +0530

    CF Round 563

---
+++ Codeforces/1174C.cpp
@@ -0,0 +1,101 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <b; i++)
+typedef long long ll;
+#define int ll
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef pair<int,int> pi;
+#define trace(x) cout<<#x<<"="<<x<<"\n";
+#define llp 1000000007
+#define mod 1000000007
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+    os << "{";
+    typename vector< T > :: const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+    os << "[";
+    typename set< T > :: const_iterator it;
+    for ( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+    os << "[";
+    typename map< F , S >::const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << it -> first << " = " << it -> second ;
+    }
+    return os << "]";
+}
+
+#define deb(x) cerr << #x << " = " << x << endl;
+vector<char> is_prime(100006, true);
+vector<int>val(100006,0);
+void pre(){
+    int n=100005;
+    int ct=1;
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i <= n; i++) {
+        if (is_prime[i]) {
+            val[i]=ct;
+            for (int j = i * i; j <= n; j += i){
+                is_prime[j] = false;
+                if(val[j]==0)
+                val[j]=ct;
+            }
+            ct++;
+        }
+    }
+}
+int a[100005];
+int32_t main(){
+	ios::sync_with_stdio(false);
+    int N;
+    cin>>N;
+    // N=1000;
+    // memset(a,0,sizeof(a));
+    pre();
+    for(int i=2;i<=N;i++){
+        cout<<val[i]<<" ";
+    }
+    cout<<"\n";
+    // for(int i=2;i<=N;i++){
+    //     for(int j=2;j<=N;j++){
+    //         if(__gcd(i,j)==1 && val[i]==val[j]){
+    //             deb(i);
+    //             deb(j);
+    //             deb("No");
+    //             return 0;
+    //         }
+    //     }
+    // }
+
+
+
+
+    return 0;
+}
