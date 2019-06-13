242cc124af278373a873ea4f34eb984d6e26e9cf
Neeraj <neeraj.171it226@nitk.edu.in>
2019-06-13 09:36:47 +0530

    SRM 760

--- Topcoder/NBAFinals.cpp
+++ Topcoder/NBAFinals.cpp
@@ -40,45 +40,4 @@ public:
         return 0;
     }
 }
-int dubsAgain1(vector <int>L, string S){
-    int W=0;
-    int R=0;
-    for(int i=0;i<S.size();i++){
-        if(L[i]==0){
-            if(S[i]!='R'){
-                W+=4;
-            }
-        }
-        else if(S[i]=='?'){
-            if(L[i]==0){
-                W+=4;
-            }
-            else{
-                W+=L[i];
-            }
-        }
-        else{
-            if(S[i]=='R'){
-                R+=L[i];
-            }
-            else{
-                W+=L[i];
-            }
-        }
-    }
-    if(W>R){
-        return 1;
-    }
-    else{
-        return 0;
-    }
-
-}
 };
-
-int main(){
-    NBAgain N;
-    cout<<N.dubsAgain({0,0,0,0,4},"RRRRW")<<"\n";
-    cout<<N.dubsAgain1({0,0,0,0,4},"RRRRW")<<"\n";
-    return 0;
-}
