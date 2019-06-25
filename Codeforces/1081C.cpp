bb30e70d7ae5898aecaa41e2e122a2a9affe8361
Neeraj <neeraj.171it226@nitk.edu.in>
2019-06-26 02:47:32 +0530

    DP and Expected Value

---
+++ Codeforces/1081C.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <b; i++)
+typedef long long ll;
+#define int ll
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef pair<int,int> pi;
+#define trace(x) cout<<#x<<"="<<x<<"\n";
+#define sz(x) (int)(x.size())
+#define mod 998244353
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+    os << "{";
+    typename vector< T > :: const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+    os << "[";
+    typename set< T > :: const_iterator it;
+    for ( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+    os << "[";
+    typename map< F , S >::const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << it -> first << " = " << it -> second ;
+    }
+    return os << "]";
+}
+
+#define deb(x) cerr << #x << " = " << x << endl;
+
+int dp[2005][2005];
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    memset(dp,0,sizeof(dp));
+    int n, m, k;
+    cin>>n>>m>>k;
+    dp[1][0]=m;
+    for(int i=2;i<=n;i++){
+        for(int j=0;j<=k;j++){
+            dp[i][j]=(((m-1)%mod)*dp[i-1][j-1]+dp[i-1][j])%mod;
+        }
+    }
+    cout<<dp[n][k]<<"\n";
+
+
+    return 0;
+}
---
+++ Codeforces/839C.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <b; i++)
+typedef long long ll;
+#define int ll
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef pair<int,int> pi;
+#define trace(x) cout<<#x<<"="<<x<<"\n";
+#define sz(x) (int)(x.size())
+#define mod 1000000007
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+    os << "{";
+    typename vector< T > :: const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+    os << "[";
+    typename set< T > :: const_iterator it;
+    for ( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+    os << "[";
+    typename map< F , S >::const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << it -> first << " = " << it -> second ;
+    }
+    return os << "]";
+}
+
+#define deb(x) cerr << #x << " = " << x << endl;
+
+vector<vector<int> >adj;
+double dfs(int x, int p){
+    int pos=0;
+    for(auto g:adj[x]){
+        if(g!=p)pos++;
+    }
+    if(pos==0)return 0;
+    double prob=1.0/pos;
+    double ans=0;
+    for(auto g:adj[x]){
+        if(g==p){
+            continue;
+        }
+        else{
+            ans+=prob*(1.0+dfs(g,x));
+        }
+    }
+    return ans;
+
+}
+int32_t main(){
+    ios::sync_with_stdio(false);
+    int n;
+    cin>>n;
+    adj.resize(n+1);
+    for(int i=0;i<(n-1);i++){
+        int A, B;
+        cin>>A>>B;
+        adj[A].PB(B);
+        adj[B].PB(A);
+    }
+    double ans=dfs(1,0);
+    printf("%0.9lf\n",ans);
+
+
+    return 0;
+}
