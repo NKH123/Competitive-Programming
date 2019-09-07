a0574cd3c353aae65ef6a3d58215f5f42f1a2ce5
Neeraj <neeraj.171it226@nitk.edu.in>
2019-09-08 02:30:42 +0530

    Modified LCA code

--- Contest_Library/lca.cpp
+++ Contest_Library/lca.cpp
@@ -1,88 +1,53 @@
-#include<algorithm>
-#include<vector>
-using namespace std;
-class LCATree
+const int N = 210000;
+ 
+const int l = 17;
+ 
+int timer;
+int up[N][l + 1];
+int bedge[N][l + 1];
+int tin[N];
+int tout[N];
+int dep[N];
+ 
+void dfs(int v, int p = 1,int pcost=0)
 {
-    public:
-        int lca(int u,int v)
-        {
-            if(!ready)
-                compute();
-            int last=-1;
-            while(u!=v)
-            {
-                if(d[u]>d[v])
-                    swap(u,v);
-                if(skip[v]!=v)
-                {
-                    last=v;
-                    v=skip[v];
-                }
-                else
-                {
-                    last=-1;
-                    v=parent[v];
-                }
-            }
-            return last==-1 ?v : last;
-        }
-        LCATree(int n, int root):n(n),root(root)
-        {
-            graph = vector<vector<int> > (n)
-            parent = vector<int> (n)
-            parent[root]=-1;
-            d = vector<int> (n);
-            size = vector<int> (n);
-            skip = vector<int> (n);
-            ready=false;
-        }
-        void add_edge(int u, int v)
-        {
-            graph[u].push_back(v);
-            graph[v].push_back(u);
-            ready=false;
-        }
-        void set_root(int newroot)
-        {
-            root=newroot;
-            ready=false;
-            parent[root]=-1;
-        }
-    private:
-        vector<vector<int> > graph;
-        vector<int> parent;
-        vector<int> d;
-        vector<int> size;
-        vector<int> skip;
-        int root,n;
-        bool ready;
-        int dfs(int v)
-        {
-            size[v]=1;
-            FOREACH(i,graph[v])
-                if(*i!=parent[v])
-                {
-                    parent[*i]=v;
-                    d[*i]=d[v]+1;
-                    size[*i]=dfs(*i);
-                    size[v]+=size[*i];
-                }
-            return size[v];
-        }
-        void skipdfs(int v)
-        {
-            skip[v]=v;
-            if(v!=root && 2*size[v] > size[parent[v]])
-                skip[v]=skip[parent[v]];
-            FOREACH(i,graf[v])
-                if(*i!=parent[v])
-                    skipdfs(*i);
-        }
-        void compute()
-        {
-            ready=true;
-            dfs(root);
-            skipdfs(root);
-        }
-};
-
+    tin[v] = timer;
+    ++timer;
+    up[v][0] = p;
+    bedge[v][0] = pcost;
+    for (int i = 1; i <= l; i++)
+    {
+        up[v][i] = up[up[v][i - 1]][i - 1];
+        bedge[v][i] = max(bedge[v][i - 1], bedge[up[v][i-1]][i - 1]);
+    }
+    for (int i = 0; i < g[v].size(); i++)
+    {
+        int to = g[v][i].first;
+        int cost = g[v][i].second;
+        if (to == p)
+            continue;
+        dep[to] = dep[v] + 1;
+        dfs(to, v, cost);
+    }
+    tout[v] = timer;
+    ++timer;
+}
+ 
+bool upper(int a, int b)
+{
+    return (tin[a] <= tin[b] && tout[a] >= tout[b]);
+}
+ 
+int lca(int a, int b)
+{
+    if (upper(a, b))
+        return a;
+    if (upper(b, a))
+        return b;
+    for (int i = l; i >= 0; --i)
+    {
+        if (!upper(up[a][i], b))
+            a = up[a][i];
+    }
+    return up[a][0];
+}
