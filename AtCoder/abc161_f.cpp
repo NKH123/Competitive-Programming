431a5375baae2c148e10b10aac2192016e5e3633
Neeraj <neeraj.deshpande3@gmail.com>
2020-04-05 23:14:59 +0530

    F upsolve. Math. observations

---
+++ AtCoder/abc161_f.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <b; i++)
+typedef long long ll;
+#define int ll
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef pair<int,int> pi;
+#define trace(x) cout<<#x<<"="<<x<<"\n";
+#define sz(x) (int)(x.size())
+#define mod 1000000007
+#define endl "\n"
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+    os << "{";
+    typename vector< T > :: const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+    os << "[";
+    typename set< T > :: const_iterator it;
+    for ( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+    os << "[";
+    typename map< F , S >::const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << it -> first << " = " << it -> second ;
+    }
+    return os << "]";
+}
+
+#define deb(x) cerr << #x << " = " << x << endl;
+int n;
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin>>n;
+    set<int>ans;
+    ans.insert(n);
+    ans.insert(n-1);
+    if(n==2){
+        cout<<1<<"\n";
+        return 0;
+    }
+    for(int i=2;i*i<=n;i++){
+            int N=n;
+            while(N%i==0){
+                N=N/i;
+            }
+            if(N%i==1){
+                ans.insert(i);
+            }
+    }
+    n--;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0){
+            ans.insert(i);
+            ans.insert(n/i);
+        }
+    }
+    cout<<ans.size()<<"\n";
+
+    return 0;
+}
